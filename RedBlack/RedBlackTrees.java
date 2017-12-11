4a13863c055b42c46cfe0c5e310efcd97b90d92b
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-12-11 00:47:17 -0500

    insert red black

--- RedBlack/RedBlackTrees.java
+++ RedBlack/RedBlackTrees.java
@@ -24,6 +24,8 @@ public class RedBlackTrees {
 		}
 	}
 
+	BST root;
+
 	public void leftRotate(BST root, BST node) {
 		BST y = node.right;
 		node.right = y.left;
@@ -38,5 +40,91 @@ public class RedBlackTrees {
 			node.parent.right = y;
 		y.left = node;
 		node.parent = y;
+	}
+
+	public void rightRotate(BST root, BST node) {
+		BST y = node.left;
+		node.left = y.right;
+		if (y.right != null)
+			y.right.parent = node;
+		y.parent = node.parent;
+		if (node.parent == null)
+			root = y;
+		else if (node == node.parent.left)
+			node.parent.left = y;
+		else
+			node.parent.right = y;
+		y.right = node;
+		node.parent = y;
+	}
+
+	public boolean search(int item) {
+		BST node = root;
+		while (node != null && node.data != item) {
+			if (item < node.data)
+				node = node.left;
+			else
+				node = node.right;
+		}
+		return node != null;
+	}
+
+	public void insert(int item) {
+		BST newnode = new BST(item);
+		BST node = root, parent = null;
+		while (node != null) {
+			if (item < node.data)
+				node = node.left;
+			else
+				node = node.right;
+			parent = node;
+		}
+		if (parent == null)
+			root = newnode;
+		else if (item < parent.data)
+			parent.left = newnode;
+		else
+			parent.right = newnode;
+		newnode.parent = parent;
+		insert_fixup(newnode);
+	}
+
+	private void insert_fixup(BST node) {
+		while (node.parent.color) {
+			if (node.parent == node.parent.parent.left) {
+				BST y = node.parent.parent.right; // uncle
+				if (y.color) {
+					node.parent.color = false; // parent = balck
+					y.color = false; // uncle = black
+					node.parent.parent.color = true; // grandparent = red
+					node = node.parent.parent;
+				} else {
+					if (node == node.parent.right) {
+						node = node.parent;
+						leftRotate(root, node);
+					}
+					node.parent.color = false; // parent = black
+					node.parent.parent.color = true; // grandparent = red
+					rightRotate(root, node);
+				}
+			} else {
+				BST y = node.parent.parent.left; // uncle
+				if (y.color) {
+					node.parent.color = false; // parent = balck
+					y.color = false; // uncle = black
+					node.parent.parent.color = true; // grandparent = red
+					node = node.parent.parent;
+				} else {
+					if (node == node.parent.left) {
+						node = node.parent;
+						rightRotate(root, node);
+					}
+					node.parent.color = false; // parent = black
+					node.parent.parent.color = true; // grandparent = red
+					leftRotate(root, node);
+				}
+			}
+			root.color = false; // root is black
+		}
 	}
 }
