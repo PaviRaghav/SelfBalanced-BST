a85ab7c1a8509b7b4dba669480a36c9127ca61d5
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-12-10 23:29:39 -0500

    first commit

---
+++ RedBlack/RedBlackTrees.java
@@ -0,0 +1,16 @@
+package SelfBalancing;
+
+import Implementation.*;
+
+/**
+ * implementation of Red-Black trees. 1. nodes are either red or black. 2. root
+ * node is black 3. leaves (NIL) are black 4. red nodes have 2 black children 5.
+ * all simple paths from (all) node to its descendant leaves have same number of
+ * black nodes
+ * 
+ * @author pavithraraghavan
+ */
+
+public class RedBlackTrees {
+
+}
