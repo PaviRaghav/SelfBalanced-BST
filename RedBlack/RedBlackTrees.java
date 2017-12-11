4dbccac847c89a3bb2525d7aadae7db713e994b7
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-12-10 23:41:56 -0500

    left rotate

--- RedBlack/RedBlackTrees.java
+++ RedBlack/RedBlackTrees.java
@@ -12,5 +12,31 @@ import Implementation.*;
  */
 
 public class RedBlackTrees {
+	class BST {
+		int data;
+		BST left, right, parent;
+		boolean color; // red=true
 
+		public BST(int i) {
+			data = i;
+			left = right = parent = null;
+			color = true;
+		}
+	}
+
+	public void leftRotate(BST root, BST node) {
+		BST y = node.right;
+		node.right = y.left;
+		if (y.left != null)
+			y.left.parent = node;
+		y.parent = node.parent;
+		if (node.parent == null)
+			root = y;
+		else if (node == node.parent.left)
+			node.parent.left = y;
+		else
+			node.parent.right = y;
+		y.left = node;
+		node.parent = y;
+	}
 }
